a87f82c1f33f10801bded2ce5d5a7735bcb19a8d
Jan <j.kozub136@gmail.com>
2021-12-19 01:16:55 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,103 @@
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  title = 'Mapa';
+
+  private mouseDownStatus = false;
+  private counter = 0;
+  private clickSwitch = false;
+
+  private currentX = 0;
+  private currentY = 0;
+
+  private startX = 0;
+  private startY = 0;
+  private offsetX = 0;
+  private offsetY = 0;
+
+  private scale = 1.0;
+
+  ngOnInit(): void {
+    const frame = $('#frame');
+    const object = $('#object');
+
+    object.on('mousedown', () => {
+      this.mouseDownStatus = true;
+    });
+
+    object.on('mouseup', () => {
+      this.mouseDownStatus = false;
+    });
+
+    frame.on('mousemove', (ev) => {
+      this.currentX = ev.clientX;
+      this.currentY = ev.clientY;
+    });
+
+    frame.on('mousewheel', (e) => {
+      if (e.originalEvent.deltaY < 0) {
+        this.scale = Math.round((this.scale + 0.1) * 100) / 100;
+      } else {
+        if (this.scale > 1.0) {
+          this.scale = Math.round((this.scale - 0.1) * 100) / 100;
+        }
+      }
+      object.css('transform', 'scale(' + this.scale + ')');
+    });
+
+    setInterval(() => {
+      if (this.mouseDownStatus === true) {
+        this.counter++;
+        this.clickSwitch = true;
+
+        if (this.counter === 1) {
+          this.startX = this.currentX;
+          this.startY = this.currentY;
+          this.offsetX = parseInt(object.css('left'), 10);
+          this.offsetY = parseInt(object.css('top'), 10);
+        }
+
+        if (this.counter > 20) {
+          onMove(this.startX, this.startY, this.currentX, this.currentY, this.offsetX, this.offsetY);
+          this.clickSwitch = false;
+        }
+      } else {
+        if (this.clickSwitch) {
+          onClick();
+        }
+        this.clickSwitch = false;
+        this.counter = 0;
+      }
+
+    }, 1);
+
+    function onMove(startX, startY, newX, newY, offsetX, offsetY): void {
+      const newLeft = offsetX + newX - startX;
+      const newTop = offsetY + newY - startY;
+
+      if (newLeft > 0 && newLeft <= 300) {
+        object.css('left', newLeft);
+      }
+
+      if (newTop > 0 && newTop <= 300) {
+        object.css('top', newTop);
+      }
+
+    }
+
+    function onClick(): void {
+      console.log('that was a nice click man');
+      console.log(document.getElementById('176inactiveG'));
+    }
+  }
+}
+
+
+
