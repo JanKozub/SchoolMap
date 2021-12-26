d6a4f8ad11fbc08cc8cef4bbf816bdfcba8f14fb
Jan <j.kozub136@gmail.com>
2021-12-26 20:31:04 +0100

    -Added database -Added database mapping -Moved from jquery functions to build in angular events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {DataService} from './data.service';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'Mapa';
 
-  private mouseDownStatus = false;
+  mouseDownStatus = false;
   private counter = 0;
   clickSwitch = false;
 
@@ -26,6 +26,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private scale = 1.0;
 
+  private object: any;
+
   message: string;
   subscription: Subscription;
 
@@ -35,75 +37,57 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscription = this.data.currentMessage.subscribe(message => this.message = message);
 
-    const frame = $('#frame');
-    const object = $('#object');
-
-    object.on('mousedown', () => {
-      this.mouseDownStatus = true;
-    });
-
-    object.on('mouseup', () => {
-      this.mouseDownStatus = false;
-    });
-
-    frame.on('mousemove', (ev) => {
-      this.currentX = ev.clientX;
-      this.currentY = ev.clientY;
-    });
-
-    frame.on('mousewheel', (e) => {
-      if (e.originalEvent.deltaY < 0) {
-        this.scale = Math.round((this.scale + 0.1) * 100) / 100;
-      } else {
-        if (this.scale > 1.0) {
-          this.scale = Math.round((this.scale - 0.1) * 100) / 100;
-        }
-      }
-      object.css('transform', 'scale(' + this.scale + ')');
-    });
-
-    setInterval(() => {
-      if (this.mouseDownStatus === true) {
-        this.counter++;
-        this.clickSwitch = true;
-
-        if (this.counter === 1) {
-          this.startX = this.currentX;
-          this.startY = this.currentY;
-          this.offsetX = parseInt(object.css('left'), 10);
-          this.offsetY = parseInt(object.css('top'), 10);
-        }
-
-        if (this.startX !== this.currentX && this.startY !== this.currentY) {
-          onMove(this.startX, this.startY, this.currentX, this.currentY, this.offsetX, this.offsetY);
-          this.clickSwitch = false;
-        }
-
-        this.sendClickStatus();
-      } else {
-        this.clickSwitch = false;
-        this.counter = 0;
-      }
-
-    }, 1);
-
-    function onMove(startX, startY, newX, newY, offsetX, offsetY): void {
-      const newLeft = offsetX + newX - startX;
-      const newTop = offsetY + newY - startY;
-
-      if (newLeft > 0 && newLeft <= 300) {
-        object.css('left', newLeft);
-      }
-
-      if (newTop > 0 && newTop <= 300) {
-        object.css('top', newTop);
-      }
-
-    }
+    this.object = $('#object');
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  onMouseMove(ev: any): void {
+    this.currentX = ev.clientX;
+    this.currentY = ev.clientY;
+
+    if (this.mouseDownStatus === true) {
+      this.counter++;
+      this.clickSwitch = true;
+
+      if (this.counter === 1) {
+        this.startX = this.currentX;
+        this.startY = this.currentY;
+        this.offsetX = parseInt(this.object.css('left'), 10);
+        this.offsetY = parseInt(this.object.css('top'), 10);
+      }
+
+      if (this.startX !== this.currentX && this.startY !== this.currentY) {
+        const newLeft = this.offsetX + this.currentX - this.startX;
+        const newTop = this.offsetY + this.currentY - this.startY;
+
+        if (newLeft > 0 && newLeft <= 300) {
+          this.object.css('left', newLeft);
+        }
+        if (newTop > 0 && newTop <= 300) {
+          this.object.css('top', newTop);
+        }
+        this.clickSwitch = false;
+      }
+
+      this.sendClickStatus();
+    } else {
+      this.clickSwitch = false;
+      this.counter = 0;
+    }
+  }
+
+  onMouseWheel(e: any): void {
+    if (e.deltaY < 0) {
+      this.scale = Math.round((this.scale + 0.1) * 100) / 100;
+    } else {
+      if (this.scale > 1.0) {
+        this.scale = Math.round((this.scale - 0.1) * 100) / 100;
+      }
+    }
+    this.object.css('transform', 'scale(' + this.scale + ')');
   }
 
   sendClickStatus(): void {
--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -2,6 +2,9 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
+import * as $ from 'jquery';
+// @ts-ignore
+import database from '../database.json';
 
 @Component({
   selector: 'app-main-view',
@@ -13,10 +16,20 @@ export class MainViewComponent implements OnInit, OnDestroy {
   clickStatus: boolean;
   subscription: Subscription;
 
+  public classesList: { title: string, ids: string[], route: string }[] = database;
+
   constructor(private data: DataService, private router: Router) {
   }
 
   ngOnInit(): void {
+    document.querySelectorAll('*[id]').forEach(e => {
+      // console.log(e.id);
+      if (e.id !== 'frame' && e.id !== 'object') {
+        const el = $('#' + e.id);
+        el.on('click', () => this.onClick(e.id));
+      }
+    });
+
     this.subscription = this.data.currentMessage.subscribe(message => {
       this.clickStatus = message.toString() === 'true';
     });
@@ -26,10 +39,15 @@ export class MainViewComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onBuildingClick(newRoute: string): void {
-    if (this.clickStatus) {
-      this.router.navigate([newRoute]).then(() => console.log('navigated to ' + newRoute));
-    }
+  onClick(id: string): void {
+    this.classesList.forEach(value => {
+      value.ids.forEach(e => {
+        if (e === id) {
+          console.log(value.title);
+          this.router.navigate([value.route]).then(() => console.log('navigated to ' + value.route));
+        }
+      });
+    });
   }
 
 }
